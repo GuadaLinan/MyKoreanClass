3db73b578e3b9579c729aec0df488ae9b6b7ca7e
Guadalupe Liñan <guada.linnan@gmail.com>
2022-05-20 20:39:59 -0300

    Agregué DOM y Eventos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 /* El simulador formará parte de la web de una institución del idioma coreano. Tiene como ojbetivo informar de manera sencilla a que grupo pertenecerá el inscripto, según su nivel*/
 
-class opcion {
+/* class opcion {
     constructor(nivel,dia,horario) {
     this.nivel = nivel;
     this.dia = dia;
@@ -26,7 +26,7 @@ if (solicitud === 1 || solicitud === 2 || solicitud ===3) {
             alert("Ha seleccionado el nivel " + opcion1.nivel + " que se cursa el día " + opcion1.dia);
             console.log(opcion1);
             horarioPregunta = prompt ("¿Quiere saber el horario de su cursado? (SI o NO)")
-            if (horarioPregunta === "SI" || horarioPregunta === "Si" || horarioPregunta === "si") {
+            if (horarioPregunta.toLowerCase() ===  "si") {
                 alert ("El horario es a las " + opcion1.horario)
             } else {}
             break;
@@ -35,7 +35,7 @@ if (solicitud === 1 || solicitud === 2 || solicitud ===3) {
             alert("Ha seleccionado el nivel " + opcion2.nivel + " que se cursa el día " + opcion2.dia);
             console.log(opcion2);
             horarioPregunta = prompt ("¿Quiere saber el horario de su cursado? (SI o NO)")
-            if (horarioPregunta === "SI" || horarioPregunta === "Si" || horarioPregunta === "si") {
+            if (horarioPregunta.toLowerCase() ===  "si") {
                 alert ("El horario es a las " + opcion2.horario)
             } else {}
             break;
@@ -44,7 +44,7 @@ if (solicitud === 1 || solicitud === 2 || solicitud ===3) {
             alert("Ha seleccionado el nivel " + opcion3.nivel + " que se cursa el dia " + opcion3.dia);
             console.log(opcion3);
             horarioPregunta = prompt ("¿Quiere saber el horario de su cursado? (SI o NO)")
-            if (horarioPregunta === "SI" || horarioPregunta === "Si" || horarioPregunta === "si") {
+            if (horarioPregunta.toLowerCase() ===  "si") {
                 alert ("El horario es a las " + opcion3.horario)
             } else {} 
              break;            
@@ -83,4 +83,19 @@ if (solicitud === 1 || solicitud === 2 || solicitud ===3) {
 }
 else {
     alert ("¡Te esperamos!")
-}
+}*/
+
+const sectionContenedor = document.getElementById("contenedor");
+
+const bienvenida = document.getElementsByClassName ("parrafoIndex") [0]
+let ingreso = document.getElementsByTagName("input")
+
+bienvenida.innerText = "Te damos la bienvenida " + ingreso
+
+const inputNombre= document.querySelector ("#nombre")
+
+inputNombre.addEventListener("click", ()=>{
+    sectionContenedor.innerText = inputNombre.value
+})
+
+inputNombre.setAttribute("type")
